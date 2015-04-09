150ab2b4204d3f38c1b88bc79f1bf933cf7e8b2d
Al Scott <al.scott@atomicobject.com>
2015-04-09 00:33:21 -0400

    Add validation checks for tweet length.

--- tweet-analyser-enterprise-ready-edition/src/Tweet.fs
+++ tweet-analyser-enterprise-ready-edition/src/Tweet.fs
@@ -1,6 +1,13 @@
 ﻿namespace TweetAnalyser
 module Tweets =
   type Tweet(text:string) =
+
+    let maxLength = 140
+
     member val Text = text with get, set
+
     member this.Length
-      with get () = this.Text.Length
+      with get() = this.Text.Length
+
+    member this.Valid
+      with get() = this.Length <= maxLength
--- tweet-analyser-enterprise-ready-edition/test/TweetTest.fs
+++ tweet-analyser-enterprise-ready-edition/test/TweetTest.fs
@@ -19,3 +19,16 @@ type ``a simple tweet`` ()=
   member x.``updates the length when the text changes`` ()=
     tweet.Text <- "new tweet"
     tweet.Length |> should equal 9
+
+[<TestFixture>]
+type ``a tweet that is too long`` ()=
+  let tweet = Tweet("A tweet that is very long and contains a large number of different words describing different thing in very long ways that are probably not that interesting anyways")
+
+  [<Test>]
+  member x.``is marked as invalid`` ()=
+    tweet.Valid |> should equal false
+
+  [<Test>]
+  member x.``is valid again if it is shortened`` ()=
+    tweet.Text <- "A valid tweet"
+    tweet.Valid |> should equal true
