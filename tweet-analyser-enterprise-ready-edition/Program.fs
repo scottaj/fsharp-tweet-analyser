5e4c0a36258d92a02ffe26af13cb2ccbe9c783a3
Al Scott <al.scott@atomicobject.com>
2015-04-09 23:31:14 -0400

    Add console program.

--- tweet-analyser-enterprise-ready-edition/Program.fs
+++ tweet-analyser-enterprise-ready-edition/Program.fs
@@ -1,4 +1,24 @@
-﻿[<EntryPoint>]
+﻿open TweetAnalyser.Tweets
+
+[<EntryPoint>]
 let main argv =
-    printfn "%A" argv
-    0 // return an integer exit code
+  System.Console.ForegroundColor <- System.ConsoleColor.Green
+  printf "Enter a tweet: "
+  let tweet = new Tweet(System.Console.ReadLine())
+
+  System.Console.Clear()
+  printfn "you entered: %s" tweet.Text
+  printfn "it is %d characters long" tweet.Length
+
+  if not tweet.Valid then
+    System.Console.ForegroundColor <- System.ConsoleColor.Red
+    printfn "it is over the maximum tweet length of 140 characters"
+    System.Console.ForegroundColor <- System.ConsoleColor.Green
+
+  let hastags = tweet.HashTags |> String.concat " "
+  printfn "it has %d hastags: %s" tweet.HashTags.Length hastags
+
+  let mentions = tweet.Mentions |> String.concat " "
+  printfn "it has %d mentions: %s" tweet.Mentions.Length mentions
+
+  0 // return an integer exit code
